73eda19c10fe43f991187904a334d915c484adc1
Marek Tews <marek.tews@gmail.com>
2023-05-29 22:03:06 +0200

    Export na github

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, reactive } from 'vue'
+import Bus from '../components/Bus.vue'
+
+const buses = reactive({ list: [] })
+
+fetch('/api/monitoring/buses')
+.then(response => {
+    if(response.status == 200)
+        return response.json()
+    else
+        return []
+})
+.then(d => {
+    console.log('Fetch buses list:', d)
+    buses.list = d
+})
+.catch( error => console.error('Fetch buses list error:', error))
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <Bus v-for="(item, index) in buses.list" :key="index" :item="item" />
+    </main>
 </template>
+
+<style scoped>
+main {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 4pt;
+}
+</style>
